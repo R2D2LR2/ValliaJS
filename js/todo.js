fffd0f825ac2fa5b147d83ae0218b2367938780d
R2D2LR2 <R2D2LR2@gmail.com>
2021-01-26 23:41:45 +0900

    #3.5

--- js/todo.js
+++ js/todo.js
@@ -3,17 +3,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-/* 다시 복습 필요! */
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   /* li 를 만들어 사용자가 입력한 내용을 버튼과 함께 html 문서에 삽입 */
   const li = document.createElement("li"),
     delBtn = document.createElement("button"),
-    span = document.createElement("span");
+    span = document.createElement("span"),
+    newId = toDos.length + 1;
+
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+
   delBtn.innerText = "❌";
   span.innerText = text;
   toDoList.appendChild(li);
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -24,8 +40,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
   if (toDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
