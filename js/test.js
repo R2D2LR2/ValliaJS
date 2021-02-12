1ab1102574dde311d4deae6bb4b62a9ae4c90138
R2D2LR2 <R2D2LR2@gmail.com>
2021-02-12 16:50:41 +0900

    day4 solution

--- js/test.js
+++ js/test.js
@@ -2,15 +2,21 @@ const title = document.querySelector("#title");
 
 const CLICKED_CLASS = "clicked";
 
+const testEvent = {
+  testMid: function testMid() {
+    console.log(console.error);
+  },
+  testTop: function () {
+    console.log(console.time);
+  },
+};
+
 function handleClick() {
-   title.classList.toggle(CLICKED_CLASS);
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.addEventListener("click", handleClick);
+  title.addEventListener("click", handleClick);
 }
 
 init();
-
-
-
