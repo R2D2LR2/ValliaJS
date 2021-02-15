3bcc97a2a5dcdbec3dca784a9bb560cd3e9762dd
R2D2LR2 <R2D2LR2@gmail.com>
2021-02-16 04:31:59 +0900

    day8-9 challenge pending

---
+++ (00) challenge/day8-9/src/index.js
@@ -0,0 +1,40 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+// import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function pendingToDo(text) {
+  //   console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  pendingToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
