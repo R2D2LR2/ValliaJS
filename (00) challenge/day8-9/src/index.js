9791662c4ea4a7d142ffc75f887a117bab355a65
R2D2LR2 <R2D2LR2@gmail.com>
2021-02-17 01:36:24 +0900

    day8-9 challenge pending

--- (00) challenge/day8-9/src/index.js
+++ (00) challenge/day8-9/src/index.js
@@ -5,18 +5,47 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = "toDos";
+const TODOS_LS = "PENDING";
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    console.log(toDo.id, li.id);
+    console.log(toDo.id, parseInt(li.id));
+    return toDo.id !== parseInt(li.id);
+  });
+  console.log(cleanToDos);
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function pendingToDo(text) {
   //   console.log(text);
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -27,8 +56,14 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    // console.log(loadedToDos);
+    const parsedToDos = JSON.parse(loadedToDos);
+    // console.log(parsedToDos);
+    parsedToDos.forEach(function (toDo) {
+      pendingToDo(toDo.text);
+    });
   }
 }
 
