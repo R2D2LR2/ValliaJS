41a9d3386ec6a8f844c0916dc12b633ee85b234a
R2D2LR2 <R2D2LR2@gmail.com>
2021-02-19 23:00:26 +0900

    day11-12 pending...

--- (00) challenge/day11-12/src/index.js
+++ (00) challenge/day11-12/src/index.js
@@ -4,16 +4,96 @@
 
 const container = document.querySelector(".container"),
   input = container.querySelector("input[type=text]"),
-  btn = document.querySelectorAll("button");
+  buttons = document.querySelectorAll("button");
 
-function clickedBtn(event) {
-  //   console.log(event.toElement.innerText);
-  console.log(event.target.value);
-  input.value = event.target.value;
-}
+let result = "";
+let operCheck = false;
+let numberCheck = true;
+let equlasCheck = true;
 
 function init() {
-  container.addEventListener("click", clickedBtn);
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      switch (button.dataset.type) {
+        case "operator":
+          const oper = button.innerText;
+          operator(oper);
+          break;
+        case "cancle":
+          clear();
+          break;
+        case "equals":
+          calc();
+          break;
+        default:
+          const number = button.innerText;
+          displayNumber(number);
+          break;
+      }
+    });
+  });
+}
+
+function displayNumber(number) {
+  operCheck = true;
+  const current = input.value;
+
+  //계산 끝난 후 초기화
+  if (equlasCheck) {
+    //연산자 눌렸을 시 계산기 값만 초기화
+    if (numberCheck) {
+      input.value = current === "0" ? number : input.value + number;
+    } else {
+      input.value = number;
+      numberCheck = true;
+    }
+    result += number;
+  } else {
+    equlasCheck = true;
+    input.value = number;
+    result = number;
+    console.log(result);
+  }
+}
+
+function calc() {
+  // 지속적으로 계산기 값 누르는 거 확인
+  if (input.value === "0") {
+    clear();
+  } else {
+    //마지막이 연산자로 끝나는 지 확인
+    if (!operCheck) {
+      alert("Calculation is unavailable because of operation.");
+    } else {
+      // 계속 눌렸을 때 변경
+      if (equlasCheck) {
+        const final = eval(result);
+        input.value = final;
+        result = "";
+        equlasCheck = false;
+        operCheck = false;
+      } else {
+        clear();
+      }
+    }
+  }
+}
+
+function operator(oper) {
+  if (!operCheck) {
+    alert("Operation Error.");
+  } else {
+    operCheck = false;
+    result += oper;
+    numberCheck = false;
+  }
+}
+
+function clear() {
+  input.value = 0;
+  result = "";
+  operCheck = false;
+  equlasCheck = true;
 }
 
 init();
